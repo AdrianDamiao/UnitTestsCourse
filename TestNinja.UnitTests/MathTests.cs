7cbe7eaacd3a21c99047f9266d8c555d52d5fb93
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-05-10 21:35:41 -0300

    Adiciona testes para o método Max de Math.cs

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -19,5 +19,44 @@ namespace TestNinja.UnitTests
             // Assert
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(2, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(1, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(1));
+        }
     }
 }
