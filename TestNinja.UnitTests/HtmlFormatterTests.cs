0158741e81b8a0fc2837f975e4eba35c47f7edf0
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-05-13 20:15:58 -0300

    Testando strings

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        private HtmlFormatter _htmlFormatterTests;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _htmlFormatterTests = new HtmlFormatter();
+        }
+
+        [Test]
+        public void HtmlFormatter_WhenCalled_ReturnsTheTextAsBold()
+        {
+            var result = _htmlFormatterTests.FormatAsBold("Some Text");
+
+            Assert.That(result, Is.EqualTo("<strong>Some Text</strong>").IgnoreCase);
+        }
+    }
+}
