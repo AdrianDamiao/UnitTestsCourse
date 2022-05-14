70d07e4e1a3515fd125cc64b7558877782783965
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-05-13 21:42:56 -0300

    Testa método void

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.LastError = "Error";
+
+            Assert.That(logger.LastError, Is.EqualTo("Error"));
+        }
+    }
+}
