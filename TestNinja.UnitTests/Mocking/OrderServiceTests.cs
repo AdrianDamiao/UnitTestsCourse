7cf582729a3b31881b0eb0354b55153d96e2cf1c
adriandamiao <adriandami_@hotmail.com>
2022-05-26 19:12:27 -0300

    Adiciona outro exemplo de mock

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        private Mock<IStorage> _storage;
+        private OrderService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _storage = new Mock<IStorage>();
+            _service = new OrderService(_storage.Object);
+        }
+
+        [Test]
+        public void PlaceOrder_WhenCalled_StorePlaceOrder()
+        {
+            var order = new Order();
+
+            _service.PlaceOrder(order);
+            _storage.Verify(s => s.Store(order));
+        }
+
+        [Test]
+        public void PlaceOrder_WhenOrderIsNull_ReturnArgumentNullException()
+        {
+
+        }
+    }
+}
