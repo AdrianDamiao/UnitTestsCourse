b605ad5b89f9883a88e27efd6ce216547f968ef1
adriandamiao <adriandami_@hotmail.com>
2022-05-26 18:13:24 -0300

    Teste de classe com dependência externa

---
+++ TestNinja.UnitTests/Mocking/VideoServicesTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServicesTests
+    {
+        [Test]
+        public void ReadVideoTitle_FileIsEmpty_ReturnErrorMessage()
+        {
+            var videoService = new VideoService();
+
+            var result = videoService.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("Error").IgnoreCase);
+        }
+    }
+}
