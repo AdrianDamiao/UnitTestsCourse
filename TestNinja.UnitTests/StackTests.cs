454a8bd8577720d6c75d49b49d12eb4c89c28fab
adriandamiao <adriandami_@hotmail.com>
2022-05-26 18:13:04 -0300

    Exercicio proposto

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Stack<string>();
+        }
+
+        [Test]
+        public void Push_WhenArgumentIsNull_ThrowArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_WhenArgumentIsValid_AddTheObjectToTheStack()
+        { 
+            _stack.Push("a");
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        { 
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithObjects_ReturnObjectOnTheTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithObjects_RemoveObjectOnTheTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            _stack.Pop();
+
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        { 
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectOnTopOfTheStack()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            var result = _stack.Peek();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveObjectOnTopOfTheStack()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            _stack.Peek();
+
+            Assert.That(_stack.Count, Is.EqualTo(3));
+        }
+    }
+}
